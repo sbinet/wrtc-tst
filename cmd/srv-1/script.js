a48307c44b04a85697dd3fbe52508ac4b532dc9d
Sebastien Binet <binet@cern.ch>
2021-12-09 20:43:21 +0100

    cmd/srv-1: add missing assets

---
+++ cmd/srv-1/script.js
@@ -0,0 +1,102 @@
+"use strict";
+
+let conn = new WebSocket('wss://' + window.location.host + '/ws')
+let pc = new RTCPeerConnection()
+
+var btnStart = document.getElementById("start-btn");
+var btnStop = document.getElementById("stop-btn");
+
+var log = msg => {
+    var el = document.getElementById('logs')
+    el.innerHTML = msg + '<br>' + el.innerHTML
+}
+
+navigator.mediaDevices.getDisplayMedia({
+        video: true,
+        audio: false
+    })
+    .then(stream => {
+        stream.getTracks().forEach(track => pc.addTrack(track, stream))
+        document.getElementById('video1').srcObject = stream
+        pc.createOffer().then(d => pc.setLocalDescription(d)).catch(log)
+    }).catch(log)
+
+
+window.startClick = () => {
+    conn.send(JSON.stringify({
+        name: 'start',
+        data: ''
+    }))
+    btnStart.disabled = true;
+    btnStop.disabled = false;
+}
+
+window.stopClick = () => {
+    log("stopping...")
+//    var video = document.getElementById("video1");
+//    var stream = video.srcObject;
+//    if (stream != null && stream.active) {
+//        log("stopping... - 1")
+//        let trks = stream.getTracks();
+//        log("stopping... - 2")
+//
+//        trks.forEach(trk => trk.stop());
+//        log("stopping... - 3")
+//        video.srcObject = null;
+//        log("stopping... - 4")
+//    }
+
+    conn.send(JSON.stringify({
+        name: 'stop',
+        data: ''
+    }))
+    log("stopping... [done]")
+
+    btnStart.disabled = false;
+    btnStop.disabled = true;
+}
+
+
+// pc.ontrack = function(event) {
+//     if (event.track.kind === 'audio') {
+//         return
+//     }
+//     var el = document.getElementById('video1')
+//     el.srcObject = event.streams[0]
+//     el.autoplay = true
+//     el.controls = true
+// }
+
+conn.onopen = () => {
+    pc.createOffer({
+        offerToReceiveVideo: true,
+        offerToReceiveAudio: false
+    }).then(offer => {
+        pc.setLocalDescription(offer)
+        conn.send(JSON.stringify({
+            name: 'offer',
+            data: JSON.stringify(offer)
+        }))
+    })
+}
+
+conn.onclose = evt => {
+    console.log('Connection closed')
+}
+
+conn.onmessage = evt => {
+    let msg = JSON.parse(evt.data)
+    if (!msg) {
+        return console.log('failed to parse msg')
+    }
+    switch (msg.name) {
+        case 'answer':
+            answer = JSON.parse(msg.data)
+            if (!answer) {
+                return console.log('failed to parse answer')
+            }
+            pc.setRemoteDescription(answer)
+    }
+}
+
+window.conn = conn
